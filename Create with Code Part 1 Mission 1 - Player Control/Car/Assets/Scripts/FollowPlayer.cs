ffdd8bf8266a85a51a471445f0580651c4e58c85
ainecal <ainec155@live.co.uk>
2024-09-30 21:37:23 +0100

    Unit 1.3 High Speed Chase
    New Functionality:
    Camera follows the vehicle down the road at a set offset distance
    
    New Concepts & Skills:
    Variables
    Data types
    Access Modifiers
    Declare and initialize variables

--- Create with Code Part 1 Mission 1 - Player Control/Car/Assets/Scripts/FollowPlayer.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Car/Assets/Scripts/FollowPlayer.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-
-    public float speed = 2;
-
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +14,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        // The vehicle will move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
