32ca5d9251da19fceb2aea53346baac0ca22f5cd
ainecal <ainec155@live.co.uk>
2024-10-01 11:23:28 +0100

    Unit 1.4 Step into the Drivers Seat
    New Functionality:
    When the player presses the up/down arrows, the vehicle will move forward and backward
    When the player presses the left/right arrows, the vehicle turns
    
    New Concepts & Skills:
    Empty objects
    Get user input
    Translate vs Rotate

--- Create with Code Part 1 Mission 1 - Player Control/Car/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Car/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 2;
+    private float speed = 30;
+    private float turnSpeed = 25;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // The vehicle will move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //The vehicle will turn around the blocks
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
