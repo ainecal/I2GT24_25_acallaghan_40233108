19fd134d920e31f8a0736b008fd0b18605c76bc5
ainecal <ainec155@live.co.uk>
2024-12-12 20:54:56 +0000

    Lesson 4.4 For-Loops For Waves

--- Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit4/Assets/Scripts/Enemy.cs
+++ Create with Code Part 2 Mission 2 - Gameplay Mechanics/Unit4/Assets/Scripts/Enemy.cs
@@ -19,5 +19,8 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+        if (transform.position.y < -10){
+            Destroy(gameObject);
+        }
     }
 }
